f98cf2eac129a6e22e71e4a19b02c32458706a4e
Aurelius Prochazka <aure@aure.com>
2015-03-09 16:30:39 -0700

    Renamed the instrument to a more generic name and introduced notes on that instrument

---
+++ Spritely/BarInstrument.swift
@@ -0,0 +1,44 @@
+//
+//  Mandolin.swift
+//  Spritely
+//
+//  Created by Simon Gladman on 08/03/2015.
+//  Copyright (c) 2015 Simon Gladman. All rights reserved.
+//
+
+import Foundation
+
+class BarInstrument: AKInstrument
+{
+    override init() {
+        super.init()
+        
+        let note = BarNote()
+        addNoteProperty(note.frequency)
+        addNoteProperty(note.amplitude)
+
+        let instrument = AKVibes()
+        instrument.frequency = note.frequency
+        instrument.amplitude = note.amplitude
+        connect(instrument)
+        
+        connect(AKAudioOutput(audioSource: instrument))
+    }
+}
+
+class BarNote: AKNote {
+    var frequency = AKNoteProperty(value: 0,    minimum: 0, maximum: 1000)
+    var amplitude = AKNoteProperty(value: 0.04, minimum: 0, maximum: 0.25)
+    
+    override init() {
+        super.init()
+        addProperty(frequency)
+        addProperty(amplitude)
+    }
+    
+    convenience init(frequency: Float, amplitude: Float) {
+        self.init()
+        self.frequency.value = frequency
+        self.amplitude.value = amplitude
+    }
+}
